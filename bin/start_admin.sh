eb19ee1374dd42dac9c428f37b966ac5ef51a5fc
Kevin Rasmussen <kj_529@hotmail.com>
2020-11-02 19:16:06 -0700

    Added requirements.txt and updated start scripts to support virtualenvs

--- bin/start_admin.sh
+++ bin/start_admin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+uwsgi=$(which uwsgi)
 script_dir="$(dirname "$(realpath "$0")")"
 cd $script_dir/../software
-uwsgi ../uwsgi/scoring_admin.ini
+$uwsgi ../uwsgi/scoring_admin.ini
 
--- bin/start_scoreboard.sh
+++ bin/start_scoreboard.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+uwsgi=$(which uwsgi)
+
 script_dir="$(dirname "$(realpath "$0")")";
 
 cd $script_dir/../software
 
-sudo uwsgi ../uwsgi/scoreboard.ini
+sudo $uwsgi ../uwsgi/scoreboard.ini
 
